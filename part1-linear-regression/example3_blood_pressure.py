00c328017af4bbbdd33153245645785a36062490
Tj23222 <tlharper7@cps.edu>
2023-12-04 10:51:55 -0600

    Pushed

--- part1-linear-regression/example3_blood_pressure.py
+++ part1-linear-regression/example3_blood_pressure.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 
 '''
 Run the program and consider the following questions:
@@ -9,11 +10,31 @@ Run the program and consider the following questions:
 '''
 
 data = pd.read_csv("part1-linear-regression/blood_pressure_data.csv")
-x = data["Age"]
-y = data["Blood Pressure"]
+x = data["Age"].values
+y = data["Blood Pressure"].values
+
+
+x = x.reshape(-1, 1)
+
+model = LinearRegression().fit(x, y)
+
+coef = round(float(model.coef_), 2)
+intercept = round(float(model.intercept_), 2)
+r_squared = model.score(x, y)
+x_predict = 43
+
+prediction = model.predict([[x_predict]])
+
+print(f"Model's Linear Equation: y = {coef}x + {intercept}")
+print(f"R Squared value: {r_squared}")
+print(f"Prediction when x is {x_predict}: {prediction}")
+
 
 #sets the size of the graph
 plt.figure(figsize=(5,4))
+
+plt.scatter(x,y, c="purple")
+plt.scatter(x_predict, prediction, c="blue")
 
 #labels axes and creates scatterplot
 plt.xlabel("Age")
@@ -21,6 +42,8 @@ plt.ylabel("Systolic Blood Pressure")
 plt.title("Systolic Blood Pressure by Age")
 plt.scatter(x, y)
 
-print("Pearson's Correlation: r = :", x.corr(y))
+plt.plot(x, coef*x + intercept, c="r", label="Line of Best Fit")
 
+
+plt.legend()
 plt.show()
